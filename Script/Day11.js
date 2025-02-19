7895b6057a758a627520da266cb92b6effcb9075
Smita <smitan@questk2.com>
2025-02-19 13:53:08 +0530

    modify

--- Script/Day11.js
+++ Script/Day11.js
@@ -10,22 +10,42 @@ function multiplicationTable(num) {
     num = parseInt(num);
     if (isNaN(num)) return "Please enter a valid number";
 
-    let table = "";
+    let table = [];
     for (let i = 1; i <= 10; i++) {
-        table += `${num} * ${i} = ${num * i}<br>`;
+        table.push(`${num} * ${i} = ${num * i}`);
     }
-    return table;
+    return table.join("\n");
 }
 
-// Function to display the day of the week based on user input
+// Function to display the day of the week based on user input using switch case
 function getDayOfWeek(dayNumber) {
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     dayNumber = parseInt(dayNumber);
-    
-    if (isNaN(dayNumber) || dayNumber < 1 || dayNumber > 7) return "Invalid day number";
-    
-    return days[dayNumber - 1];
+
+    if (isNaN(dayNumber) || dayNumber < 1 || dayNumber > 7) {
+        return "Invalid day number";
+    }
+
+    switch (dayNumber) {
+        case 1: return "Sunday";
+        break;
+        case 2: return "Monday";
+        break;
+        case 3: return "Tuesday";
+        break;
+        case 4: return "Wednesday";
+        break;
+        case 5: return "Thursday";
+        break;
+        case 6: return "Friday";
+        break;
+        case 7: return "Saturday";
+        break;
+        default: return "Invalid day number"; // This won't be reached due to the earlier check
+        break;
+     }
+
 }
+
 
 // Function to find the sum of the first 10 natural numbers
 function sumOfFirst10NaturalNumbers() {
@@ -47,30 +67,3 @@ function reverseString(str) {
     }
     return reversed;
 }
-
-// Event listeners for the HTML buttons
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("checkEvenOddBtn").addEventListener("click", function () {
-        const num = document.getElementById("numberInput").value;
-        document.getElementById("evenOddResult").innerText = checkEvenOdd(num);
-    });
-
-    document.getElementById("multiplicationTableBtn").addEventListener("click", function () {
-        const num = document.getElementById("multiplicationInput").value;
-        document.getElementById("multiplicationResult").innerHTML = multiplicationTable(num);
-    });
-
-    document.getElementById("dayOfWeekBtn").addEventListener("click", function () {
-        const dayNum = document.getElementById("dayInput").value;
-        document.getElementById("dayResult").innerText = getDayOfWeek(dayNum);
-    });
-
-    document.getElementById("sumBtn").addEventListener("click", function () {
-        document.getElementById("sumResult").innerText = sumOfFirst10NaturalNumbers();
-    });
-
-    document.getElementById("reverseStringBtn").addEventListener("click", function () {
-        const str = document.getElementById("stringInput").value;
-        document.getElementById("reverseResult").innerText = reverseString(str);
-    });
-});
